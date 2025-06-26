475dd764d6df1010392e87605eb8018711d9ba83
karolinyB <ana.silva@baymetrics.com.br>
2025-06-26 15:08:27 -0300

    chore: configure Jest to handle image imports using mocks folder

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,10 @@ module.exports = {
   transform: {
     "^.+\\.(j|t)sx?$": "babel-jest",
   },
-  transformIgnorePatterns: [
-    "/node_modules/(?!(@hookform|zod)/)",
-  ],
+  transformIgnorePatterns: ["/node_modules/(?!(@hookform|zod)/)"],
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
     "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
