5497dc246c5642b18b346c2838ae97aea92b3056
karolinyB <ana.silva@baymetrics.com.br>
2025-06-27 04:54:53 -0300

    chore: update index.css import path

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./frontend/routes/App";
-import "./frontend/style/index.css";
+import "./style/index.css";
 
 export default function Root(props) {
   return (
