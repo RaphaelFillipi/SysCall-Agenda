f933ab4136c64b648ddfa17ed9638f1724a8e4f0
karolinyB <ana.silva@baymetrics.com.br>
2025-06-29 01:43:17 -0300

    feat: add search components to page Contact

--- src/frontend/pages/Contacts.tsx
+++ src/frontend/pages/Contacts.tsx
@@ -1,3 +1,34 @@
+import { SearchButton } from "../../components/Button/SearchButton";
+import { SearchInput } from "../../components/Input/Search/SearchInput";
+import iconOrderDesc from "../../assets/icons/icon-order-desc.svg";
+import iconFilter from "../../assets/icons/icon-filter.svg";
+
 export function Contact() {
-  return <div className="relative max-h-screen p-6">Página de Contato</div>;
+  return (
+    <div className="relative flex flex-col max-h-screen overflow-hidden p-6">
+      <div>
+        <SearchInput />
+      </div>
+      <div className="py-10 space-y-5">
+        <div className="flex flex-row space-x-4 justify-center">
+          <SearchButton
+            icon={iconOrderDesc}
+            title="Ordenar"
+            alt="Ícone de Ordenar Contatos"
+            bgcolor={"bg-white"}
+            textColor={"text-green-dark"}
+            sizeIcon={"w-7 h-7"}
+          />
+          <SearchButton
+            icon={iconFilter}
+            title="Filtrar"
+            alt="Ícone de Filtrar Contatos"
+            bgcolor={"bg-white"}
+            textColor={"text-green-dark"}
+            sizeIcon={"w-6 h-4"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
