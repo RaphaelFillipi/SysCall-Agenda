72998b5bd2662567d7c855a958b3bb053c213701
karolinyB <ana.silva@baymetrics.com.br>
2025-07-14 16:26:13 -0300

    feat: add ContactCard component to Contact page

--- src/frontend/pages/Contacts.tsx
+++ src/frontend/pages/Contacts.tsx
@@ -1,4 +1,5 @@
 import { SearchInput } from "../components/Input/SearchInput";
+import { ContactCard } from "../components/Card/ContactCard";
 import { SearchButtonGroupMobile } from "../components/Button/SearchButtonGroupMobile";
 import { SearchButtonGroupDesktop } from "../components/Button/SearchButtonGroupDesktop";
 
@@ -7,8 +8,8 @@ export function Contact() {
     <div className="flex flex-col md:flex-row w-full h-screen">
       <div className="p-2">Menu</div>
       <div className="relative flex w-full overflow-hidden pt-5 md:pt-[50px] justify-center">
-        <div className="flex flex-col h-full w-full md:w-[70%] max-w-[1000px]">
-          <div className="px-4 md:px-1">
+        <div className="flex flex-col h-full md:w-[70%] max-w-[1000px]">
+          <div className="px-3 md:px-1">
             <SearchInput />
           </div>
 
@@ -17,6 +18,18 @@ export function Contact() {
           </div>
           <div className="w-full hidden md:block md:px-1">
             <SearchButtonGroupDesktop />
+          </div>
+
+          <div className="overflow-auto hide-scrollbar flex-1">
+            <div className="flex-1 w-full md:w-5/6 lg:w-full grid lg:grid-cols-2 gap-x-16 gap-y-5 px-3 md:px-1 pt-1 pb-5 mx-auto">
+              <ContactCard />
+              <ContactCard />
+              <ContactCard />
+              <ContactCard />
+              <ContactCard />
+              <ContactCard />
+              <ContactCard />
+            </div>
           </div>
         </div>
       </div>
