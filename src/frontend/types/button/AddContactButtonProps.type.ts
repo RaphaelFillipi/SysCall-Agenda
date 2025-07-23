3e672a6768f184d0bee7a13c221320b21288f37c
karolinyB <ana.silva@baymetrics.com.br>
2025-07-23 14:04:34 -0300

    types: extend AddContactButtonProps with native button attributes

--- src/frontend/types/button/AddContactButtonProps.type.ts
+++ src/frontend/types/button/AddContactButtonProps.type.ts
@@ -1,4 +1,5 @@
-export interface AddContactButtonProps {
+export interface AddContactButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   icon: string;
   alt: string;
