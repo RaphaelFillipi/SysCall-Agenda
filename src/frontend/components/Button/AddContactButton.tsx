bfe7ffdd2e17dd0817bdc4b21f148e0b3145e31d
karolinyB <ana.silva@baymetrics.com.br>
2025-07-23 13:27:38 -0300

    refactor: wrap AddContactButton button label with a div for layout improvements

--- src/frontend/components/Button/AddContactButton.tsx
+++ src/frontend/components/Button/AddContactButton.tsx
@@ -1,14 +1,22 @@
 import { AddContactButtonProps } from "../../types/button/AddContactButtonProps.type";
 
-export function AddContactButton({ icon, title, alt }: AddContactButtonProps) {
+export function AddContactButton({
+  icon,
+  title,
+  alt,
+  ...rest
+}: AddContactButtonProps) {
   return (
     <button
       className={
         "inline-flex items-center justify-center py-2 rounded-[9px] space-x-[10px] bg-green-dark text-white-primary border-green-dark  font-extrabold px-3"
       }
+      {...rest}
     >
       <img src={icon} alt={alt} className="w-[20px] h-[20px]" />
-      <span className="text-sm">{title}</span>
+      <div>
+        <span className="text-sm">{title}</span>
+      </div>
     </button>
   );
 }
