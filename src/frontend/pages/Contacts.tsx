bc501dd000965248af48372fd5a3f0391fcb2e49
karolinyB <ana.silva@baymetrics.com.br>
2025-07-23 14:16:40 -0300

    feat: add ContactModal component for add contact

--- src/frontend/pages/Contacts.tsx
+++ src/frontend/pages/Contacts.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { SearchInput } from "../components/Input/SearchInput";
 import { SearchButtonGroupMobile } from "../components/Button/SearchButtonGroupMobile";
 import { SearchButtonGroupDesktop } from "../components/Button/SearchButtonGroupDesktop";
+import { ContactModal } from "../components/Modal/ContactModal";
+import { ModalInput } from "../components/Modal/ModalInput";
+import { ContactModalButton } from "../components/Modal/ContactModalButton";
 
 export function Contact() {
+  const [showAddContactModal, setShowAddContactModal] =
+    useState<boolean>(false);
+
   return (
     <div className="flex flex-col md:flex-row w-full h-screen">
       <div className="p-2">Menu</div>
@@ -13,13 +20,45 @@ export function Contact() {
           </div>
 
           <div className="w-full md:hidden px-4">
-            <SearchButtonGroupMobile />
+            <SearchButtonGroupMobile
+              onClick={() => setShowAddContactModal(!showAddContactModal)}
+            />
           </div>
           <div className="w-full hidden md:block md:px-1">
-            <SearchButtonGroupDesktop />
+            <SearchButtonGroupDesktop
+              onClick={() => setShowAddContactModal(!showAddContactModal)}
+            />
           </div>
         </div>
       </div>
+
+      {showAddContactModal && (
+        <ContactModal
+          className="bg-white-primary/20 backdrop-blur-[1px]"
+          title="Adicionar Novo Contato"
+        >
+          <div className="flex flex-col space-y-3">
+            <ModalInput label="Nome" type="text" textColor="text-black" />
+
+            <ModalInput label="Telefone" type="text" textColor="text-black" />
+          </div>
+
+          <div className="flex flex-row justify-end space-x-6 pt-6">
+            <ContactModalButton
+              title="Cancelar"
+              bgColor="bg-transparent"
+              textColor="text-green-dark font-extrabold"
+              onClick={() => setShowAddContactModal(!showAddContactModal)}
+            />
+
+            <ContactModalButton
+              title="Adicionar"
+              bgColor="bg-green-dark"
+              textColor="text-white-primary font-bold"
+            />
+          </div>
+        </ContactModal>
+      )}
     </div>
   );
 }
